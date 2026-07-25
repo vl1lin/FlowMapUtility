4e355a6c593657fc7b2f4578a8b5e1e5a6b9de27
vl1lin <v1adbarten@mail.ru>
2026-07-26 01:43:47 +0300

    Написано ядро вызова расчета для получения кода режима течения, не тестировалось

---
+++ Run_Core/process_manager.py
@@ -0,0 +1,53 @@
+import multiprocessing as mp
+
+import numpy as np
+
+from typing import TYPE_CHECKING, Iterator
+
+from worker_settings import init_worker, worker_function
+
+
+if TYPE_CHECKING:
+    from Correlation.model_abs import IFlowModel
+    from Grid_Generation.grid_info import GridInfo
+
+
+class ProcessManager:
+    """
+    Класс для управления процессами расчета кодов режима потока.
+    :param worker_model: Модель для расчета кодов режима потока
+    :param grid: Информация о сетке расчета
+    :param n_workers: Количество рабочих процессов
+    """
+    def __init__(self, worker_model: "IFlowModel", grid: "GridInfo", n_workers: int | None = None):
+        self.worker_model = worker_model
+        self.grid = grid
+        self.n_workers = n_workers
+
+    def run(self):
+        """
+        Запускает расчет кодов режима потока
+        """
+        with mp.Pool(processes=self._count_workers(), initializer=init_worker, initargs=(self.worker_model,)) as pool:
+            results = pool.imap_unordered(worker_function, self._prepare_tasks())
+
+        code_pattern_grid = np.zeros([self.grid.resolution, self.grid.resolution], dtype=np.int32)
+        for i, row in results:
+            code_pattern_grid[i] = row
+
+        return code_pattern_grid
+
+    def _count_workers(self) -> int:
+        """
+        Определяет количесвто рабочих процессов
+        """
+        if self.n_workers is None:
+            return max(1, mp.cpu_count() - 1)
+        return self.n_workers
+
+    def _prepare_tasks(self) -> Iterator[tuple[int, np.ndarray, np.ndarray]]:
+        """
+        Подготавливает задачи для расчета в процессах
+        """
+        tasks = ((i, self.grid.vsl_2d[i:], self.grid.vsg_2d[i:]) for i in range(self.grid.resolution))
+        return tasks
---
+++ Run_Core/worker_settings.py
@@ -0,0 +1,29 @@
+from typing import TYPE_CHECKING
+
+import numpy as np
+
+if TYPE_CHECKING:
+    from Correlation.model_abs import IFlowModel
+
+_worker_model: "IFlowModel"
+
+
+def init_worker(model: "IFlowModel"):
+    """
+    Сохраняет в памяти процесса объект модели для расчета кодов режима потока
+    :param model: Модель для расчета кодов режима потока
+    """
+    global _worker_model
+    _worker_model = model
+
+def worker_function(args: tuple[int, np.ndarray, np.ndarray]) -> tuple[int, np.ndarray]:
+    """
+    Основная функция - воркер которая будет запускаться в процессах
+    :param args: Кортеж с номером строки, массивами vsl и vsg
+    """
+    i, vsl, vsg = args
+    results = np.zeros(len(vsl), dtype=np.int32)
+    for j in range(len(vsl)):
+        results[i] = _worker_model.get_pattern_code(vsl[j], vsg[j])
+
+    return i, results
