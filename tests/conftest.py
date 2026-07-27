4af5a66eeeca0a3380673b275945e50ec5031cbb
vl1lin <v1adbarten@mail.ru>
2026-07-28 02:19:31 +0300

    Протестировал ядро расчета сетки кодов, изменил архитектуру папки с тестами

---
+++ tests/conftest.py
@@ -0,0 +1,117 @@
+import pytest
+import numpy as np
+from Grid_Generation.grid_info import GridInfo
+from Run_Core.process_manager import ProcessManager
+from Data_Block_1.adapter_for_data import PipeParamsAdapter, FluidParamsAdapter, SystemParamsAdapter
+from Correlation.ansari import AnsariModel
+from Grid_Generation.grid_generator import GridGenerator
+from unittest.mock import Mock
+import math
+
+@pytest.fixture
+def pipe_adapter_not_valid():
+    pipe_adapter = PipeParamsAdapter("wrong", "wrong", "wrong") #type: ignore
+    return pipe_adapter
+
+@pytest.fixture
+def pipe_adapter_all_not_SI():
+    pipe_adapter = PipeParamsAdapter(1000, 0.1, 45 * math.pi / 180)
+    return pipe_adapter
+
+@pytest.fixture
+def pipe_adapter_all_SI():
+    pipe_adapter = PipeParamsAdapter(0.1, 0.1, 45)
+    return pipe_adapter
+
+@pytest.fixture
+def system_adapter_not_valid():
+    system_adapter = SystemParamsAdapter("", "") #type: ignore
+    return system_adapter
+
+@pytest.fixture
+def system_adapter_all_not_SI():
+    system_adapter = SystemParamsAdapter(1, 70)
+    return system_adapter
+
+@pytest.fixture
+def system_adapter_all_SI():
+    system_adapter = SystemParamsAdapter(1 * 1e6, 274)
+    return system_adapter
+
+
+@pytest.fixture
+def fluid_adapter_not_valid():
+    fluid_adapter = FluidParamsAdapter("", "", "", "", "") #type: ignore
+    return fluid_adapter
+
+@pytest.fixture
+def fluid_adapter_all_SI():
+    fluid_adapter = FluidParamsAdapter(1000, 7, 0.1, 0.5, 40)
+    return fluid_adapter
+
+@pytest.fixture
+def info_for_ansari_correlation():
+    pipe = PipeParamsAdapter(0.062, 0.00005, 90).to_si()
+    fluid = FluidParamsAdapter(
+        800,
+        50,
+        0.001,
+        0.00001,
+        0.01
+    ).to_si()
+    ansari_model = AnsariModel(pipe, fluid)
+    return ansari_model
+
+@pytest.fixture
+def creating_Pipe():
+    pipe = Mock(angle = 80.0)
+    return pipe
+
+@pytest.fixture
+def create_grid_generator() -> GridGenerator:
+    grid = GridGenerator(
+        vsl_range=(0.1, 0.5),
+        vsg_range=(10.0, 15.0),
+        resolution=10,
+        log_scale=True,
+    )
+    return grid
+
+@pytest.fixture
+def create_small_grid_generator() -> GridGenerator:
+    grid = GridGenerator(
+        vsl_range=Mock(),
+        vsg_range=Mock(),
+        resolution=3,
+        log_scale=Mock(),
+    )
+    return grid
+
+@pytest.fixture
+def create_lin_grid_generator() -> GridGenerator:
+    grid = GridGenerator(
+        vsl_range=(0.1, 0.5),
+        vsg_range=(10.0, 15.0),
+        resolution=10,
+        log_scale=False,
+    )
+    return grid
+
+@pytest.fixture
+def create_emergency_grid_generator() -> GridGenerator:
+    grid = GridGenerator(
+        vsl_range=(-0.1, 0.5),
+        vsg_range=(-1.0, 5.0),
+        resolution=10,
+        log_scale=True,
+    )
+    return grid
+
+@pytest.fixture
+def core_beginer(info_for_ansari_correlation):
+    vsl_1d = np.array([0.1, 0.2, 0.3])
+    vsl_2d = np.ones((3, 1)) * vsl_1d
+    vsg_1d = np.flip(np.array([1.0, 2.0, 3.0]))
+    vsg_2d = vsg_1d[:, np.newaxis] * np.ones((3,3))
+    grid_info = GridInfo(vsl_1d, vsg_1d, vsl_2d, vsg_2d, 3)
+    return ProcessManager(info_for_ansari_correlation, grid_info)
---
+++ tests/test_for_core/test_for_core.py
@@ -0,0 +1,55 @@
+from asyncio import taskgroups
+from typing import TYPE_CHECKING
+import numpy as np
+import pytest
+import multiprocessing as mp
+from Run_Core.worker_settings import worker_function, init_worker
+from unittest.mock import Mock
+
+
+if TYPE_CHECKING:
+    from Run_Core.process_manager import ProcessManager
+
+
+def test_core_run(core_beginer: "ProcessManager") -> None:
+    manager: "ProcessManager" = core_beginer
+    assert manager._count_workers() == 7
+    tasks = [(0, [0.1, 0.2, 0.3], [3. for _ in range(3)]),
+        (1, [0.1, 0.2, 0.3], [2. for _ in range(3)]),
+        (2, [0.1, 0.2, 0.3], [1. for _ in range(3)])]
+    for tsk_my, tsk_manager in zip(tasks, manager._prepare_tasks()):
+        i, vsl, vsg = tsk_my
+        i_manager, vsl_manager, vsg_manager = tsk_manager
+        assert i == i_manager
+        assert np.allclose(vsl, vsl_manager)
+        assert np.allclose(vsg, vsg_manager)
+
+def test_worker_function() -> None:
+    ansari = Mock(get_pattern_code=Mock(return_value=105))
+    i: int = 0
+    vsl = np.array([0.1 for _ in range(3)])
+    vsg = np.array([1.0 for _ in range(3)])
+    args = (i, vsl, vsg)
+    init_worker(ansari)
+    res_i, result = worker_function(args)
+    assert res_i == i
+    assert len(result) == 3
+    assert np.all(result == 105)
+
+
+def test_for_core(core_beginer: "ProcessManager") -> None:
+    manager: "ProcessManager" = core_beginer
+    grid = [
+        [105, 105, 105],
+        [105, 105, 105],
+        [103, 103, 103]
+    ]
+    code_grid = manager.run()
+    assert code_grid is not None
+    assert code_grid.shape == (manager.grid.resolution, manager.grid.resolution)
+    assert np.all(code_grid == np.array(grid))
+
+if __name__ == '__main__':
+    # Если ты запускаешь через pytest, он сам поймет эту конструкцию.
+    # Если запускаешь просто python test_file.py, сработает этот блок.
+    pytest.main([__file__])
